3f090acb84cf2f69143605b05629cb876af447c6
Dave Conway-Jones <conway@uk.ibm.com>
2021-02-03 13:20:52 +0000

    Add second output (non optional as yet)
    start of accepting input shape commented out.

--- geofence.js
+++ geofence.js
@@ -33,15 +33,15 @@ module.exports = function(RED) {
             var loc = undefined;
             send = send || function() { node.send.apply(node,arguments) }
 
-            if (msg.hasOwnProperty("payload") && msg.payload.hasOwnProperty("action")){
+            if (msg.hasOwnProperty("payload") && msg.payload.hasOwnProperty("action")) {
                 if (msg.payload.action === "send") {
-                    console.log(node)
                     var m = {payload: {
                         name: node.name||"GeoFence",
                         layer: "geofence",
                         fillOpacity: 0.05,
                         fillColor: "#404040",
-                        clickable: true
+                        clickable: true,
+                        editable: true
                     }};
                     if (node.inside === "false") { m.payload.color = "#009100"; }
                     if (node.inside === "both") { m.payload.color = "#919100"; }
@@ -53,8 +53,18 @@ module.exports = function(RED) {
                         m.payload.lon = node.centre.longitude;
                         m.payload.radius = node.radius;
                     }
-                    send(m);
+                    send([null,m]);
                 }
+                // if (msg.payload.action === "draw" && msg.payload.name === node.name) {
+                //     if (node.mode === "circle") {
+                //         node.centre.latitude = msg.payload.lat;
+                //         node.centre.longitude = msg.payload.lon;
+                //         node.radius = msg.payload.radius;
+                //     }
+                //     if (node.mode === "polyline") {
+                //         node.points = msg.payload.area.map( function(p) { return {latitude:p.lat, longitude:p.lng}} );
+                //     }
+                // }
             }
 
             if (msg.hasOwnProperty('location') && msg.location.hasOwnProperty('lat') && msg.location.hasOwnProperty('lon')) {
@@ -83,7 +93,7 @@ module.exports = function(RED) {
                 }
 
                 if (inout && (node.inside === "true")) {
-                    if (node.name) { 
+                    if (node.name) {
                         if (!msg.location) {
                             msg.location = {};
                         }
@@ -144,9 +154,10 @@ module.exports = function(RED) {
                     return;
                 }
             } else {
-                //no location
+                //no location - so meh
                 if (done) {
-                    done("No location found in message")
+                    done();
+                    // done("No location found in message")
                 }
                 // else {
                 //     node.error("No location found in message", msg);
